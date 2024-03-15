5e6359785621117acb5b3c1684a6fb48ab1dd852
jojusuar <jojusuar@espol.edu.ec>
2024-03-15 12:51:27 -0500

    fixed infinite recursion on equivalent calculation

--- Note.js
+++ Note.js
@@ -1,49 +1,52 @@
 class Note {
-    constructor(symbol, flat, sharp) {
+    constructor(symbol, flat, sharp, calculate) {
         const notes = ["C", "D", "E", "F", "G", "A", "B"];
         this.flat = flat;
         this.sharp = sharp;
         let index = notes.indexOf(symbol);
         let halfStep = false;
-        if(flat){
+        if (flat) {
             this.symbol = symbol + "b";
-            if(symbol === "C" || symbol === "F"){
-                halfStep = true;
+            if (calculate) {
+                if (symbol === "C" || symbol === "F") {
+                    halfStep = true;
+                }
+                if (symbol === "C") {
+                    this.equivalent = new Note("B", false, !halfStep, false);
+                }
+                else {
+                    this.equivalent = new Note(notes[index - 1], false, !halfStep, false);
+                }
             }
-            if(symbol === "C"){
-                this.equivalent = new Note("B", false, !halfStep);
-            }
-            else{
-                this.equivalent = new Note(notes[index-1], false, !halfStep);
-            }
-            
         }
-        else if(sharp){
+        else if (sharp) {
             this.symbol = symbol + "#";
-            if(symbol === "B" || symbol === "E"){
-                halfStep = true;
-            }
-            if(symbol === "B"){
-                this.equivalent = new Note("C", !halfStep, false);
-            }
-            else{
-                this.equivalent = new Note(notes[index+1], !halfStep, false);
+            if(calculate){
+                if (symbol === "B" || symbol === "E") {
+                    halfStep = true;
+                }
+                if (symbol === "B") {
+                    this.equivalent = new Note("C", !halfStep, false, false);
+                }
+                else {
+                    this.equivalent = new Note(notes[index + 1], !halfStep, false, false);
+                }
             }
         }
-        else{
+        else {
             this.symbol = symbol;
             this.equivalent = null;
         }
     }
 }
 
-function noteBuilder(symbol, flat, sharp){
-    let note = new Note(symbol, flat, sharp);
-    if(note.equivalent !== null){
+function noteBuilder(symbol, flat, sharp) {
+    let note = new Note(symbol, flat, sharp, true);
+    if (note.equivalent !== null) {
         note.equivalent.equivalent = note;
     }
-    else{
-        note.equivalent = new Note("N/A", false, false);
+    else {
+        note.equivalent = new Note("N/A", false, false, false);
     }
-    return note; 
+    return note;
 }
