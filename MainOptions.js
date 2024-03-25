fcd41f153fc1ba98f603da48c446395e1cafe14d
jojusuar <jojusuar@espol.edu.ec>
2024-03-24 22:54:43 -0500

    vastly improved UI

--- MainOptions.js
+++ MainOptions.js
@@ -9,29 +9,33 @@ optionButtons.forEach(button => {
     button.addEventListener('click', () => {
         optionButtons.forEach(btn => {
             btn.classList.remove('selected');
-            btn.style.backgroundColor = 'white';
+            btn.style.backgroundColor = 'rgb(39, 40, 41)';
         });
         button.classList.add('selected');
-        button.style.backgroundColor = 'yellow';
+        button.style.backgroundColor = 'rgb(70, 70, 70)';
     });
 });
+let defaultNote = document.getElementById("CButton");
+defaultNote.style.backgroundColor = 'rgb(70, 70, 70)';
+let defaultAlter = document.getElementById("naturalButton");
+defaultAlter.style.backgroundColor = 'rgb(70, 70, 70)';
 let defaultToggle = document.getElementById("ScaleButtons");
 let defaultOpt = document.getElementById("scaleOption");
-defaultOpt.style.backgroundColor = 'yellow';
+defaultOpt.style.backgroundColor = 'rgb(70, 70, 70)';
 defaultToggle.style.display = "block";
 let noteButtons = document.querySelectorAll('.toggleA-button');
 let alterButtons = document.querySelectorAll('.toggleB-button');
-let noteValue;
+let noteValue = "C";
 let flat = false;
 let sharp = false;
 noteButtons.forEach(button => {
     button.addEventListener('click', () => {
         noteButtons.forEach(btn => {
             btn.classList.remove('selected');
-            btn.style.backgroundColor = 'white';
+            btn.style.backgroundColor = 'rgb(39, 40, 41)';
         });
         button.classList.add('selected');
-        button.style.backgroundColor = 'blue';
+        button.style.backgroundColor = 'rgb(70, 70, 70)';
         noteValue = button.getAttribute('data-value');
     });
 });
@@ -39,10 +43,10 @@ alterButtons.forEach(button => {
     button.addEventListener('click', () => {
         alterButtons.forEach(btn => {
             btn.classList.remove('selected');
-            btn.style.backgroundColor = 'white';
+            btn.style.backgroundColor = 'rgb(39, 40, 41)';
         });
         button.classList.add('selected');
-        button.style.backgroundColor = 'blue';
+        button.style.backgroundColor = 'rgb(70, 70, 70)';
         let alterValue = button.getAttribute('data-value');
         if (alterValue === 'FLAT') {
             flat = true;
@@ -60,8 +64,8 @@ alterButtons.forEach(button => {
 });
 
 let scaleDiv = document.getElementById("ScaleButtons");
-let scaleDropdowns = '<select id="scaleSelector" onchange="handleScale()"><option value="DIATONIC" selected>Diatonic (natural)</option><option value="PENTATONIC">Pentatonic</option><option value="HARMONIC">Harmonic</option><option value="MELODIC_ASC">Melodic (ascending)</option><option value="MELODIC_DESC">Melodic (descending)</option></select><select id="modeSelector"><option value=0 selected>I. Ionian (major)</option><option value=1>II. Dorian</option><option value=2>III. Phrygian</option><option value=3>IV. Lydian</option><option value=4>V. Mixolydian</option><option value=5>VI. Aeolian (minor)</option><option value=6>VII. Locrian</option></select>';
-let scaleDropdownsForHarmonicCircle = '<select id="scaleSelector" onchange="handleScale()"><option value="DIATONIC" selected>Diatonic (natural)</option><option value="HARMONIC">Harmonic</option><option value="MELODIC_ASC">Melodic (ascending)</option><option value="MELODIC_DESC">Melodic (descending)</option></select><select id="modeSelector"><option value=0 selected>I. Ionian (major)</option><option value=1>II. Dorian</option><option value=2>III. Phrygian</option><option value=3>IV. Lydian</option><option value=4>V. Mixolydian</option><option value=5>VI. Aeolian (minor)</option><option value=6>VII. Locrian</option></select>';
+let scaleDropdowns = '<select id="scaleSelector" class="dropdown" onchange="handleScale()"><option value="DIATONIC" selected>Diatonic (natural)</option><option value="PENTATONIC">Pentatonic</option><option value="HARMONIC">Harmonic</option><option value="MELODIC_ASC">Melodic (ascending)</option><option value="MELODIC_DESC">Melodic (descending)</option></select><select id="modeSelector" class="dropdown"><option value=0 selected>I. Ionian (major)</option><option value=1>II. Dorian</option><option value=2>III. Phrygian</option><option value=3>IV. Lydian</option><option value=4>V. Mixolydian</option><option value=5>VI. Aeolian (minor)</option><option value=6>VII. Locrian</option></select>';
+let scaleDropdownsForHarmonicCircle = '<select id="scaleSelector" class="dropdown" onchange="handleScale()"><option value="DIATONIC" selected>Diatonic (natural)</option><option value="HARMONIC">Harmonic</option><option value="MELODIC_ASC">Melodic (ascending)</option><option value="MELODIC_DESC">Melodic (descending)</option></select><select id="modeSelector" class="dropdown"><option value=0 selected>I. Ionian (major)</option><option value=1>II. Dorian</option><option value=2>III. Phrygian</option><option value=3>IV. Lydian</option><option value=4>V. Mixolydian</option><option value=5>VI. Aeolian (minor)</option><option value=6>VII. Locrian</option></select>';
 
 let divOutput = document.getElementById("output");
 
@@ -81,10 +85,10 @@ function toggleElements(buttonID, divID) {
     closePopup();
     let selected = document.getElementById(divID);
     selected.style.display = 'block';
-    if(buttonID === 'scaleOption'){
-        scaleDiv.innerHTML = scaleDropdowns + '<br> Select a root note, a scale and a greek mode from above, then hit the build button to generate the scale <br> <button type="button" id="scaleButton" onclick="printScale()">Build scale</button>';
+    if (buttonID === 'scaleOption') {
+        scaleDiv.innerHTML = scaleDropdowns + '<br> Select a root note, a scale and a greek mode from above, then hit the build button to generate the scale <br> <button type="button" id="scaleButton" class="output-button" onclick="printScale()">Build scale</button>';
     }
-    else if(buttonID === 'harmonicCircleOption'){
-        scaleDiv.innerHTML = scaleDropdownsForHarmonicCircle + '<br> Select a tonic chord, a scale and a greek mode from above, then hit the build button to generate the harmonic circle <br> <button type="button" id="harmonicCircleButton" onclick="printHarmonicCircle()">Build harmonic circle</button> <br> Click on any chord to reveal its components <br> Show all available tensions <label class="switch"><input type="checkbox" onclick="toggleTensions()"><span class="slider round"></span></label>';
+    else if (buttonID === 'harmonicCircleOption') {
+        scaleDiv.innerHTML = scaleDropdownsForHarmonicCircle + '<br> Select a tonic chord, a scale and a greek mode from above, then hit the build button to generate the harmonic circle <br> <button type="button" id="harmonicCircleButton" class="output-button" onclick="printHarmonicCircle()">Build harmonic circle</button> <br> Click on any chord to reveal its components <br> Show all available tensions <label class="switch"><input type="checkbox" onclick="toggleTensions()"><span class="slider round"></span></label>';
     }
 };
