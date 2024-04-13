fdedb28169d388ce9818a1cd4ccf2d9802fc55d9
jojusuar <jojusuar@espol.edu.ec>
2024-04-12 19:47:17 -0500

    defined graph behavior

--- ChordFinder.js
+++ ChordFinder.js
@@ -4,6 +4,7 @@ let addNoteButton = document.getElementById('addNoteButton');
 let deleteNoteButton = document.getElementById('deleteNoteButton');
 deleteNoteButton.style.display = 'none';
 let selectedNotes = [];
+let noteGraph = new Graph(true);
 
 let foundChord = '<h1> *still cooking* </h1>';
 
@@ -21,13 +22,29 @@ function deleteNote() {
 }
 
 function findChord() {
-    divOutput.innerHTML = foundChord;
+    //divOutput.innerHTML = foundChord;
+    divOutput.innerHTML = noteGraph.toString();
     //TODO
+    //1.build directed graph of notes
+    //2.search for 7 semitones edge. if exists, source of the edge is the root
+    //3.search for 6 semitones edge. if exists, vertex with shortest path to the other avoiding the edges that bind them is the root
+    //4.search for all 8 semitones edge. if exists, pick the first involved note to appear in the array as the root
+    //5.if none of the above, just pick first note in the array as root.
 }
 
 addNoteButton.addEventListener('click', function () {
     let myNote = noteBuilder(noteValue, flat, sharp);
-    selectedNotes.push(myNote);
+    let duplicate = false;
+    for (let note of selectedNotes) {
+        if (myNote.equals(note)) {
+            duplicate = true;
+            break;
+        }
+    }
+    if(!duplicate){
+        selectedNotes.push(myNote);
+        noteGraph.cartesianAddition(myNote);
+    }
     if (selectedNotes.length != 0) {
         deleteNoteButton.style.display = 'inline-block';
         if (selectedNotes.length > 2) {
@@ -36,25 +53,25 @@ addNoteButton.addEventListener('click', function () {
                 addNoteButton.style.display = 'none';
             }
         }
-        else{
+        else {
             clearOutput();
         }
     }
     displaySelectedNotes();
 });
 
-deleteNoteButton.addEventListener('click', function(){
+deleteNoteButton.addEventListener('click', function () {
     deleteNote();
     displaySelectedNotes();
     if (selectedNotes.length > 2) {
         findChord();
     }
-    else{
+    else {
         clearOutput();
     }
     if (selectedNotes.length < 7) {
         addNoteButton.style.display = 'inline-block';
-        if(selectedNotes.length == 0){
+        if (selectedNotes.length == 0) {
             deleteNoteButton.style.display = 'none';
         }
     }
